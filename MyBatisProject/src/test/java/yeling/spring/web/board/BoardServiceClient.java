f58c71a6089da493dcba1853f55bd78d295803c5
kimyealin <ksv3232@naver.com>
2022-07-28 10:14:37 +0900

    2022/07/28 2

---
+++ MyBatisProject/src/test/java/yeling/spring/web/board/BoardServiceClient.java
@@ -0,0 +1,24 @@
+package yeling.spring.web.board;
+
+import java.util.List;
+
+import yeling.spring.web.board.impl.BoardDAO;
+
+public class BoardServiceClient {
+
+	public static void main(String[] args) {
+		BoardDAO boardDAO = new BoardDAO();
+		BoardVO vo = new BoardVO();
+		vo.setTitle("myBatis 테스트");
+		vo.setWriter("기매링");
+		vo.setContent("myBatis 내용");
+		boardDAO.insertBoard(vo);
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		for(BoardVO board: boardList) {
+			System.out.println("====> " + board.toString());
+		}
+	}
+
+}
